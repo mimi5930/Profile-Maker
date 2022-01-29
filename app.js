8634af3e4352a738bdcdcf8dadbed620c7e63b0b
Mike <mikej.miller440@gmail.com>
2022-01-29 17:22:40 -0600

    wrote TODOs in app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const inqurier = require('inquirer');
+
+// TODO: add inquirer prompt
+// TODO: prompt manager name, Employee ID, email, and Office number
+// TODO Then prompt whether they'd like to enter in an intern, engineer or finish building my team
+// TODO: Engineer: engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+// TODO: Intern: intern’s name, ID, email, and school, and I am taken back to the menu
+// TODO: finish building my team
+// TODO: prompt HTML generation
