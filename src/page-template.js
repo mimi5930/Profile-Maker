88a88f14d16cbc0fc78cdbf06d79f2034d284ae5
Mike <mikej.miller440@gmail.com>
2022-01-29 17:26:30 -0600

    Added todos

---
+++ src/page-template.js
@@ -0,0 +1,3 @@
+// TODO: Generate HTML of team roster
+// TODO: when email address is clicked, default email program opens and populates the TO field of the email with the address
+// TODO: clicking on GitHub Username opens profile in new tab
